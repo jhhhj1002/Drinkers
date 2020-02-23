1ac7419abe6d137a636495b0819adae5100fa618
_zz_hyun <jhhhj1002@hanmail.net>
2020-02-23 16:24:15 +0900

    first commit

---
+++ drinkers/src/main/java/com/example/demo/DrinkersApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkersApplication.class, args);
+	}
+
+}
