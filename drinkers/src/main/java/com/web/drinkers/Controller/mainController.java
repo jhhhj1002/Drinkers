b633d24bd1dc2846a8cfad4b1445ee52df995a42
_zz_hyun <jhhhj1002@hanmail.net>
2020-03-07 22:59:30 +0900

    mainController 추

---
+++ drinkers/src/main/java/com/web/drinkers/Controller/mainController.java
@@ -0,0 +1,18 @@
+package com.web.drinkers.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class mainController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView goLogin() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index");
+		return mv;
+	}
+
+}
