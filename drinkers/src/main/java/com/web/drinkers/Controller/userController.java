2e7b542090a325334a167d440a14cdad996b33a9
_zz_hyun <jhhhj1002@hanmail.net>
2020-03-10 01:41:08 +0900

    회원가입시 아이디 중복 체크 Controller 추가

--- drinkers/src/main/java/com/web/drinkers/Controller/userController.java
+++ drinkers/src/main/java/com/web/drinkers/Controller/userController.java
@@ -4,8 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.web.drinkers.Service.userService;
@@ -16,6 +19,7 @@ public class userController {
 	
 	@Autowired
 	private userService userservice;
+	private userVo uservo = new userVo();
 	private static final Logger logger = LogManager.getLogger(userController.class.getName());
 	
 	
@@ -26,13 +30,13 @@ public class userController {
 		
 		String message="";
 		ModelAndView mv = new ModelAndView();
-		boolean member_id = userservice.selectUserId(user);
-		boolean member_info = userservice.selectUserInfo(user);
+		boolean is_member_id = userservice.selectUserId(user);
+		boolean is_member_info = userservice.selectUserInfo(user);
 		
-		if(member_id == false) { 
+		if(is_member_id == false) { 
 			message = "존재하지 않는 아이디 입니다";
 		}
-		else if(member_info == false){ 
+		else if(is_member_info == false){ 
 			message = "아이디와 패스워드가 일치하지 않습니다";				
 		}
 		
@@ -48,6 +52,19 @@ public class userController {
 		
 		userservice.insertUser(user);
 		return "redirect:/";
+	}
+	
+	// 회원가입시 아이디 중복 확인	
+	@ResponseBody
+	@RequestMapping(value = "/do_check_id", method = RequestMethod.GET)
+	public boolean doCheckId(@RequestParam(value="userId") String userId) throws Exception {
+		logger.info("ID 정보" + userId);
+		
+		uservo.setId(userId);
+		boolean member_id = userservice.selectUserId(uservo);
+		logger.info("ID 중복 정보" + member_id);
+		
+		return member_id;
 	}
 	
 	// 로그인 성공시 메인페이지 이동
