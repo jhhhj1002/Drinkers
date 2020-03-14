6a3aa8fcf47273385b23f6750077186e2944f9a7
_zz_hyun <jhhhj1002@hanmail.net>
2020-03-15 00:01:07 +0900

    로그 추가

--- drinkers/src/main/java/com/web/drinkers/Controller/mainController.java
+++ drinkers/src/main/java/com/web/drinkers/Controller/mainController.java
@@ -1,5 +1,7 @@
 package com.web.drinkers.Controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,8 +10,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class mainController {
 	
+	private static final Logger logger = LogManager.getLogger(mainController.class.getName());
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView goLogin() {
+		logger.info("로그인/회원가입 페이지 이동");
+		
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("login/index");
 		return mv;
