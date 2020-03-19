75a5a178a2ca9a5c130afafae1866857e5ad6614
_zz_hyun <jhhhj1002@hanmail.net>
2020-03-19 23:46:30 +0900

    파일 업로드시 controller테스

--- drinkers/src/main/java/com/web/drinkers/Controller/foodController.java
+++ drinkers/src/main/java/com/web/drinkers/Controller/foodController.java
@@ -1,5 +1,14 @@
 package com.web.drinkers.Controller;
 
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
@@ -8,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.web.drinkers.Vo.foodrecipeVo;
 
 @Controller
 public class foodController {
@@ -26,8 +37,56 @@ public class foodController {
 	// food recipe 등록시
 	@ResponseBody
 	@RequestMapping(value = "/do_food_recipe_regist", method = RequestMethod.POST)
-	public boolean doFoodRecipeUpload() throws Exception {
+	public boolean doFoodRecipeUpload(foodrecipeVo foodrecipe,HttpServletRequest request) throws Exception {
 		logger.info("Food recipe 등록");
+		logger.info("Food recipe 이미지 파일명" + foodrecipe.getImges());
+		logger.info("Food recipe 이미지 파일명" + foodrecipe.getImges().get(0));
+		logger.info("Food recipe 제목" + foodrecipe.getTitle());
+		
+		ServletContext context = request.getServletContext();
+
+        String appPath = context.getRealPath("/");
+
+        System.out.println(appPath);
+        
+		
+		String path = "src/main/resources/static/upload/foodrecipe/";
+		String imagePath = request.getServletContext().getRealPath("resources/static/upload/foodrecipe");
+		String title = foodrecipe.getTitle();
+		String originalfileName = foodrecipe.getImges().get(0).getOriginalFilename();
+		
+		String sPath = request.getSession().getServletContext().getRealPath("/filepathInContext");
+		System.out.println(sPath);
+
+		
+		String fileName = "upload";
+
+		
+		URL url = this.getClass().getResource("/");
+		
+		System.out.println(url);
+		
+		File files = new File(".");
+        String rootPath = files.getAbsolutePath();
+        System.out.println("현재 프로젝트의 경로 : "+rootPath );
+
+
+    	String contextRoot = new HttpServletRequestWrapper(request).getRealPath("/");
+    	 System.out.println("현재 프로젝트의 경로 : "+contextRoot );
+
+        String pathsss = "/Users/ijihyeon/git/Drinkers/drinkers/target/classes/static/upload/"+originalfileName;
+		
+//		File dest = new File("/Users/ijihyeon/git/Drinkers/drinkers/src/main/resources/static/upload/foodrecipe/" + originalfileName);
+		File dest = new File(pathsss);
+//		foodrecipe.getImges().get(0).transferTo(dest);
+	
+		foodrecipe.getImges().get(0).transferTo(dest); 
+		
+	
+		
+
+//		/drinkers/src/main/java/com/web/drinkers/Controller/userController
+//		/drinkers/src/main/resources/static/upload/foodrecipe
 
 		return false;
 	}
