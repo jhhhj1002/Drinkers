ca3b8a879196e943ace499e9d16b4de360e81ab3
_zz_hyun <jhhhj1002@hanmail.net>
2020-03-21 00:20:39 +0900

    images 리스트 변수 삭제 , id 변수 추가

--- drinkers/src/main/java/com/web/drinkers/Vo/foodVo.java
+++ drinkers/src/main/java/com/web/drinkers/Vo/foodVo.java
@@ -1,18 +1,13 @@
 package com.web.drinkers.Vo;
 
-import java.util.List;
-
-import org.springframework.web.multipart.MultipartFile;
-
 import lombok.Data;
 
 @Data
-public class foodrecipeVo {
+public class foodVo {
 	
 	private String title;
-	private List<MultipartFile> imges;
-//	private String imges;
+	private String id;
 	private String url;
-	private String desc;
+	private String exp;
 	
 }
