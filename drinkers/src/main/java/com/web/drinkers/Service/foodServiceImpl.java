31097ebcc8b7c831b1a05d28bd0e9ad85fb5ff78
_zz_hyun <jhhhj1002@hanmail.net>
2020-03-26 23:29:10 +0900

    files 반복문으로 db 에 추가

--- drinkers/src/main/java/com/web/drinkers/Service/foodServiceImpl.java
+++ drinkers/src/main/java/com/web/drinkers/Service/foodServiceImpl.java
@@ -1,19 +1,29 @@
 package com.web.drinkers.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.web.drinkers.Dao.fileDao;
 import com.web.drinkers.Dao.foodDao;
+import com.web.drinkers.Vo.fileVo;
 import com.web.drinkers.Vo.foodVo;
 
 public class foodServiceImpl implements foodService{
 	
 	@Autowired
 	private foodDao fooddao;
+	@Autowired
+	private fileDao filedao;
 
 	@Override
-	public void insertfood(foodVo food) { // 이미지 리스트 db에 insert 추가
+	public void insertfoodrecipe(foodVo food,List<fileVo> fileList) { // db에 food,file 정보 insert
 		// TODO Auto-generated method stub
 		fooddao.insertFoodRecipe(food);
+		
+		for(fileVo file : fileList) {
+			filedao.insertFile(file);
+		}
 	}
 
 }
