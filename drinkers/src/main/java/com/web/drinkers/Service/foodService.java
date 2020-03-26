762acf154ed6c138e431d94a9c6c938f80c56018
_zz_hyun <jhhhj1002@hanmail.net>
2020-03-26 23:28:56 +0900

    method name 변경, 매개변수 추가

--- drinkers/src/main/java/com/web/drinkers/Service/foodService.java
+++ drinkers/src/main/java/com/web/drinkers/Service/foodService.java
@@ -1,13 +1,16 @@
 package com.web.drinkers.Service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.web.drinkers.Vo.foodVo;
+import com.web.drinkers.Vo.fileVo;
 
 @Service
 public interface foodService {
 	
-	public void insertfood(foodVo food); // db 에 food 정보 insert
+	public void insertfoodrecipe(foodVo food,List<fileVo> fileList);// db에 food,file 정보 insert
 	
 	// 이미지 리스트 insert
 	// 이미지 리스트 return 
