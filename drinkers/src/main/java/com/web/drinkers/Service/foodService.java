ad04f9020e5c6cde6786a019e9ba51f9830715f0
_zz_hyun <jhhhj1002@hanmail.net>
2020-03-31 21:18:30 +0900

    selectAllFoodRecipeInfo 함수 추가

--- drinkers/src/main/java/com/web/drinkers/Service/foodService.java
+++ drinkers/src/main/java/com/web/drinkers/Service/foodService.java
@@ -1,6 +1,7 @@
 package com.web.drinkers.Service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ public interface foodService {
 	
 	public void insertfoodrecipe(foodVo food,List<fileVo> fileList);// db에 food,file 정보 insert
 	
+	public List<Map<String, Object>> selectAllFoodRecipeInfo();
 	// 이미지 리스트 insert
 	// 이미지 리스트 return 
 
