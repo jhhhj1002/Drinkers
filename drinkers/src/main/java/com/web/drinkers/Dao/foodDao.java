489ba774c74b023b81cdee494ad8157360711ccc
_zz_hyun <jhhhj1002@hanmail.net>
2020-03-31 21:17:43 +0900

    selectAllFoodRecipeInfo 메소드 추가

--- drinkers/src/main/java/com/web/drinkers/Dao/foodDao.java
+++ drinkers/src/main/java/com/web/drinkers/Dao/foodDao.java
@@ -1,11 +1,13 @@
 package com.web.drinkers.Dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.web.drinkers.Vo.foodVo;
-import com.web.drinkers.Vo.userVo;
 
 @Repository
 public class foodDao {
@@ -15,8 +17,13 @@ public class foodDao {
 
 	// db 에 foodrecipe 정보 insert
 	public int insertFoodRecipe(foodVo foodrecipe) {
-		String query = "INSERT INTO FoodRecipe(title,id,url,exp) VALUES(?,?,?,?)";
-		return jdbcTemplate.update(query, foodrecipe.getTitle(),foodrecipe.getId(), foodrecipe.getUrl(),foodrecipe.getExp());
+		String query = "INSERT INTO FoodRecipe(title,id,url,exp,img) VALUES(?,?,?,?,?)";
+		return jdbcTemplate.update(query, foodrecipe.getTitle(),foodrecipe.getId(), foodrecipe.getUrl(),foodrecipe.getExp(),foodrecipe.getImg());
+	}
+	
+	public List<Map<String, Object>> selectAllFoodRecipeInfo() {
+		String query = "SELECT * From FoodRecipe";
+		return jdbcTemplate.queryForList(query);
 	}
 
 }
