7a46fd9f4992a4c26b9efcf9713875fb76b0f197
_zz_hyun <jhhhj1002@hanmail.net>
2020-04-18 22:03:02 +0900

    recommended_order 함수 추가

--- drinkers/src/main/resources/static/js/food_recipe.js
+++ drinkers/src/main/resources/static/js/food_recipe.js
@@ -17,7 +17,6 @@ $("#close_modal").click(function () {
 
 // 최신순 클릭시 재정렬
 function lastest_order() {
-
 	$.ajax({
 		url : "do_lastest_order",
 		type : 'GET',
@@ -31,17 +30,51 @@ function lastest_order() {
 							+ "<figcaption>"+ item.title +"</figcaption> </figure>";
 			
 				$('#image').append(temp);
-			});
-			
+			});			
 		},
 		error : function(xhr, status, data) {
 			alert(xhr + " : " + status);
 		}
 	});
-
 }
 
-
+//추천순 클릭시 재정렬
+function recommended_order() {
+	
+	var reco_arr = new Array();
+	
+	$.ajax({
+		url : "do_lastest_order",
+		type : 'GET',
+		success : function(data) {
+			
+			$.each(data, function(index, item){	
+				if (!localStorage[item.title]) {
+					localStorage[item.title] = 0;
+				}
+				reco_arr[index] = new Array(item.title,item.img,localStorage[item.title]);
+			});	
+			
+			reco_arr.sort(function(a, b) {
+		        return b[2] - a[2];
+		    });
+			
+			$('#image').empty();
+			
+			for(var i in reco_arr){
+				 var temp = "<figure id='"+ reco_arr[i][0] + "'>"
+					+ "<a href=\"go_food_recipe_detail?title="+ reco_arr[i][0] +"\">"
+					+ "<img src=\"uploads/"+ reco_arr[i][0] + "|"+ reco_arr[i][1] +"\"> </a> "
+					+ "<figcaption>"+ reco_arr[i][0] +"</figcaption> </figure>";
+		
+				 $('#image').append(temp);				 
+			 }				
+		},
+		error : function(xhr, status, data) {
+			alert(xhr + " : " + status);
+		}
+	});
+}
 
 
 
