ce62f0323d4bcb417b12da5a0aaf16a67c68ad08
_zz_hyun <jhhhj1002@hanmail.net>
2020-04-18 22:19:13 +0900

    goAlcoholRecipe 함수 추가

--- drinkers/src/main/java/com/web/drinkers/Controller/alcoholController.java
+++ drinkers/src/main/java/com/web/drinkers/Controller/alcoholController.java
@@ -1,8 +1,27 @@
 package com.web.drinkers.Controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class alcoholController {
+	
+	private static final Logger logger = LogManager.getLogger(foodController.class.getName());
+	
+	@RequestMapping(value = "/go_alcohol_recipe", method = RequestMethod.GET)
+	public ModelAndView goAlcoholRecipe() {
+		logger.info("Alcohol Recipe 페이지 이동");
+	
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("alcohol/alcohol_recipe");
+		return mv;
+	}
 
 }
